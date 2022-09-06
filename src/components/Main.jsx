98b0bd07a40cce8fcccfcf594f73603a849c8407
Askin <asknkskk@gmail.com>
2022-09-06 12:04:14 +0300

    inputs completed on local

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import weather from "../assets/Weather.png";
 
 const Main = () => {
+  var maxNumber = 45;
+  var randomNumber = Math.floor(Math.random() * maxNumber + 1);
+  const [inputs, setInputs] = useState({
+    facility_id: "",
+    year: "",
+    activity: "",
+    fuel: "",
+    vehicle: "",
+    amount: "",
+    unit: "",
+  });
+  const handleChange = (e) => {
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
+  };
+  console.log(inputs);
   return (
     <>
       <div className="bg-white rounded" style={{ height: "103px" }}>
@@ -27,20 +42,147 @@ const Main = () => {
               <div className="m-3">
                 <label htmlFor="facility-id">Facility Id</label>
                 <select
-                  className="form-select arrow-color "
+                  className="form-select arrow-color p-2 w-75 "
                   id="facility-id"
+                  aria-label="Default select example"
+                  name="facility_id"
+                  style={{
+                    border: "2px solid #0D1840",
+                    borderRadius: "8px 8px 35px 8px",
+                  }}
+                  onChange={handleChange}
+                  required
+                >
+                  <option value="">Seçiniz</option>
+                  <option value={randomNumber}>{randomNumber}</option>
+                </select>
+              </div>
+              <div className="m-3">
+                <label htmlFor="year">Year</label>
+                <select
+                  className="form-select arrow-color p-2 w-75 "
+                  id="year"
+                  name="year"
                   aria-label="Default select example"
                   style={{
                     border: "2px solid #0D1840",
                     borderRadius: "8px 8px 35px 8px",
                   }}
+                  onChange={handleChange}
                   required
                 >
                   <option value="">Seçiniz</option>
-                  <option value="1">One</option>
+                  <option value={new Date().getFullYear()}>
+                    {new Date().getFullYear()}
+                  </option>
+                  <option value={new Date().getFullYear() - 1}>
+                    {new Date().getFullYear() - 1}
+                  </option>
+                  <option value={new Date().getFullYear() - 2}>
+                    {new Date().getFullYear() - 2}
+                  </option>
+                  <option value={new Date().getFullYear() - 3}>
+                    {new Date().getFullYear() - 3}
+                  </option>
+                  <option value={new Date().getFullYear() - 4}>
+                    {new Date().getFullYear() - 4}
+                  </option>
+                </select>
+              </div>
+              <div className="m-3">
+                <label htmlFor="activity-type">Activity Type</label>
+                <select
+                  className="form-select arrow-color p-2 w-75 "
+                  id="activity-type"
+                  name="activity"
+                  aria-label="Default select example"
+                  style={{
+                    border: "2px solid #0D1840",
+                    borderRadius: "8px 8px 35px 8px",
+                  }}
+                  onChange={handleChange}
+                  required
+                >
+                  <option value="">Seçiniz</option>
+                  <option value="4">Distance Activity</option>
+                  <option value="5">Fuel Use</option>
+                </select>
+              </div>
+              <div className="m-3">
+                <label htmlFor="fuel-source">Fuel Source</label>
+                <select
+                  className="form-select arrow-color p-2 w-75 "
+                  id="fuel-source"
+                  name="fuel"
+                  aria-label="Default select example"
+                  style={{
+                    border: "2px solid #0D1840",
+                    borderRadius: "8px 8px 35px 8px",
+                  }}
+                  onChange={handleChange}
+                  required
+                >
+                  <option value="">Seçiniz</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                </select>
+              </div>
+              <div className="m-3">
+                <label htmlFor="vehicle-type">Vehicle Type</label>
+                <select
+                  className="form-select arrow-color p-2 w-75 "
+                  id="vehicle-type"
+                  name="vehicle"
+                  aria-label="Default select example"
+                  style={{
+                    border: "2px solid #0D1840",
+                    borderRadius: "8px 8px 35px 8px",
+                  }}
+                  onChange={handleChange}
+                  required
+                >
+                  <option value="">Seçiniz</option>
+                  <option value="1">1</option>
                   <option value="2">Two</option>
                   <option value="3">Three</option>
                 </select>
+              </div>
+              <div className="m-3">
+                <div className="input-group mb-3 w-75">
+                  <input
+                    type="text"
+                    className="form-control"
+                    aria-label="text input with dropdown button"
+                    name="amount"
+                    style={{
+                      border: "2px solid #0D1840",
+                      borderRight: "none",
+                      borderRadius: "8px 0 0 8px",
+                    }}
+                    value={inputs.amount}
+                    placeholder="Giriniz"
+                    onChange={handleChange}
+                  />
+                  <select
+                    className="form-select arrow-color p-2  "
+                    id="facility-id"
+                    aria-label="Default select example"
+                    name="unit"
+                    style={{
+                      border: "2px solid #0D1840",
+                      borderLeft: "none",
+                      borderRadius: "0 8px 35px 0",
+                    }}
+                    onChange={handleChange}
+                    required
+                  >
+                    <option value="">Seçiniz</option>
+                    <option value="1">oo</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                  </select>
+                </div>
               </div>
             </div>
           </div>
@@ -54,9 +196,9 @@ const Main = () => {
               </p>
             </div>
             <div className="input-group mb-3 w-75">
-              <div class="input-resim">
+              <div className="input-resim">
                 <img src={weather} alt="mavi-amblem" />
-                <div class="centered">
+                <div className="centered">
                   CO<sub>2</sub>
                 </div>
               </div>
@@ -68,9 +210,9 @@ const Main = () => {
               />
             </div>
             <div className="input-group mb-3 w-75">
-              <div class="input-resim">
+              <div className="input-resim">
                 <img src={weather} alt="mavi-amblem" />
-                <div class="centered">
+                <div className="centered">
                   CH<sub>4</sub>
                 </div>
               </div>
@@ -82,9 +224,9 @@ const Main = () => {
               />
             </div>
             <div className="input-group mb-3 w-75">
-              <div class="input-resim">
+              <div className="input-resim">
                 <img src={weather} alt="mavi-amblem" />
-                <div class="centered">
+                <div className="centered">
                   N<sub>2</sub>O
                 </div>
               </div>
@@ -96,9 +238,9 @@ const Main = () => {
               />
             </div>
             <div className="input-group mb-3 w-75">
-              <div class="input-resim">
+              <div className="input-resim">
                 <img src={weather} alt="mavi-amblem" />
-                <div class="centered">
+                <div className="centered">
                   CO<sub>2</sub>e
                 </div>
               </div>
